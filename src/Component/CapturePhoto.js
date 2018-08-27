9b5439c0c8fd9a1e3cff0f4a0897dd4a7e1ee171
pushpendra65 <pushpendra.khandelwal@kuliza.com>
2018-08-27 10:52:10 +0530

    photo-upload wip

---
+++ src/Component/CapturePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View} from 'react-native'
+import PhotoUpload from 'react-native-photo-upload'
+
+const CapturePhoto = () => {
+    render(
+        <PhotoUpload/>
+    )
+}
+
+export default CapturePhoto
